5cf94babc94d1b07e9e084e18b14fa950a8cdb4e
yash <yash.rezolut@gmail.com>
2023-06-19 13:08:06 +0530

    Init application.properties

--- src/main/java/com/management/attendance/entities/Attendance.java
+++ src/main/java/com/management/attendance/entities/Attendance.java
@@ -1,5 +1,23 @@
 package com.management.attendance.entities;
 
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="Attendance")
+@Getter
+@Setter
 public class Attendance {
+    @Id
+    private Long id;
+    private Date date;
+
+    private Date
+
+    @OneToOne
+    @JoinColumn(name = "emp_id")
+    private Employee employee;
 }
